60110d1c3e2cf6bc3d79a51d54a3fe7ef0dde7c3
NDEGE <82872168+bndegeke@users.noreply.github.com>
2024-03-05 11:29:43 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,13 @@
+
+name=input('enter username: ')
+age=input('enter age ')
+location=input('enter location: ')
+print('Hello ', name, 'you are ', age, ' years old and live in ', location)
+
+
+
+
+
+
+
+# Hello [name], you are [age] years old and live in [location]
